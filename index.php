1b5534961f399abc868b8f5b5d9ca34c38acc519
Ben Bowler <ben@benbowler.com>
2012-11-25 19:43:51 +0000

    Complete schedule.php

--- index.php
+++ index.php
@@ -1,64 +1,422 @@
 <?php
 
-echo "Hello";
+/**
+ * This sample app is provided to kickstart your experience using Facebook's
+ * resources for developers.  This sample app provides examples of several
+ * key concepts, including authentication, the Graph API, and FQL (Facebook
+ * Query Language). Please visit the docs at 'developers.facebook.com/docs'
+ * to learn more about the resources available to you
+ */
 
-die(getenv("MONGOHQ_URL"));
+// Provides access to app specific values such as your app id and app secret.
+// Defined in 'AppInfo.php'
+require_once('AppInfo.php');
 
-/* mongodb */
-
-try {
-// connect to MongoHQ assuming your MONGOHQ_URL environment
-// variable contains the connection string
-$connection_url = getenv("MONGOHQ_URL");
-
-// create the mongo connection object
-$m = new Mongo($connection_url);
-
-// extract the DB name from the connection path
-$url = parse_url($connection_url);
-$db_name = preg_replace('/\/(.*)/', '$1', $url['path']);
-
-// use the database we connected to
-$db = $m->selectDB($db_name);
-
-echo "<h2>Collections</h2>";
-echo "<ul>";
-
-// print out list of collections
-$cursor = $db->listCollections();
-$collection_name = "";
-foreach( $cursor as $doc ) {
-  echo "<li>" .  $doc->getName() . "</li>";
-  $collection_name = $doc->getName();
+// Enforce https on production
+if (substr(AppInfo::getUrl(), 0, 8) != 'https://' && $_SERVER['REMOTE_ADDR'] != '127.0.0.1') {
+  header('Location: https://'. $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+  exit();
 }
-echo "</ul>";
 
-// print out last collection
-if ( $collection_name != "" ) {
-  $collection = $db->selectCollection($collection_name);
-  echo "<h2>Documents in ${collection_name}</h2>";
+// This provides access to helper functions defined in 'utils.php'
+require_once('utils.php');
 
-  // only print out the first 5 docs
-  $cursor = $collection->find();
-  $cursor->limit(5);
-  echo $cursor->count() . ' document(s) found. <br/>';
-  foreach( $cursor as $doc ) {
-    echo "<pre>";
-    var_dump($doc);
-    echo "</pre>";
+
+/*****************************************************************************
+ *
+ * The content below provides examples of how to fetch Facebook data using the
+ * Graph API and FQL.  It uses the helper functions defined in 'utils.php' to
+ * do so.  You should change this section so that it prepares all of the
+ * information that you want to display to the user.
+ *
+ ****************************************************************************/
+
+require_once('sdk/src/facebook.php');
+
+$facebook = new Facebook(array(
+  'appId'  => AppInfo::appID(),
+  'secret' => AppInfo::appSecret(),
+  'sharedSession' => true,
+  'trustForwarded' => true,
+));
+
+$user_id = $facebook->getUser();
+if ($user_id) {
+  try {
+    // Fetch the viewer's basic information
+    $basic = $facebook->api('/me');
+  } catch (FacebookApiException $e) {
+    // If the call fails we check if we still have a user. The user will be
+    // cleared if the error is because of an invalid accesstoken
+    if (!$facebook->getUser()) {
+      header('Location: '. AppInfo::getUrl($_SERVER['REQUEST_URI']));
+      exit();
+    }
   }
+
+  // This fetches some things that you like . 'limit=*" only returns * values.
+  // To see the format of the data you are retrieving, use the "Graph API
+  // Explorer" which is at https://developers.facebook.com/tools/explorer/
+  $likes = idx($facebook->api('/me/likes?limit=4'), 'data', array());
+
+  // This fetches 4 of your friends.
+  $friends = idx($facebook->api('/me/friends?limit=4'), 'data', array());
+
+  // And this returns 16 of your photos.
+  $photos = idx($facebook->api('/me/photos?limit=16'), 'data', array());
+
+  // Here is an example of a FQL call that fetches all of your friends that are
+  // using this app
+  $app_using_friends = $facebook->api(array(
+    'method' => 'fql.query',
+    'query' => 'SELECT uid, name FROM user WHERE uid IN(SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1'
+  ));
 }
 
-// disconnect from server
-$m->close();
-} catch ( MongoConnectionException $e ) {
-die('Error connecting to MongoDB server');
-} catch ( MongoException $e ) {
-die('Mongo Error: ' . $e->getMessage());
-} catch ( Exception $e ) {
-die('Error: ' . $e->getMessage());
-}
+// Fetch the basic info of the app that they are using
+$app_info = $facebook->api('/'. AppInfo::appID());
+
+$app_name = idx($app_info, 'name', '');
 
 
+// Mongo
+# get the mongo db name out of the env
+$mongo_url = parse_url(getenv("MONGOHQ_URL"));
+$dbname = str_replace("/", "", $mongo_url["path"]);
+
+# connect
+$m   = new Mongo(getenv("MONGOHQ_URL"));
+$db  = $m->$dbname;
+$col = $db->videos;
+
+var_dump($col->skip(rand(0, 49))->find());
+
+?>
+<!DOCTYPE html>
+<html xmlns:fb="http://ogp.me/ns/fb#" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=2.0, user-scalable=yes" />
+
+    <title><?php echo he($app_name); ?></title>
+
+    <!-- Bootstrap -->
+    <script type="text/javascript" src="assets/javascript/jquery-1.7.1.min.js"></script>
+    <script type="text/javascript" src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-responsive.min.css" type="text/css" />
+
+    <!--<link rel="stylesheet" href="assets/stylesheets/screen.css" media="Screen" type="text/css" />-->
+    <!--<link rel="stylesheet" href="assets/stylesheets/mobile.css" media="handheld, only screen and (max-width: 480px), only screen and (max-device-width: 480px)" type="text/css" />-->
+
+    <!--[if IEMobile]>
+    <link rel="stylesheet" href="mobile.css" media="screen" type="text/css"  />
+    <![endif]-->
+
+    <!-- These are Open Graph tags.  They add meta data to your  -->
+    <!-- site that facebook uses when your content is shared     -->
+    <!-- over facebook.  You should fill these tags in with      -->
+    <!-- your data.  To learn more about Open Graph, visit       -->
+    <!-- 'https://developers.facebook.com/docs/opengraph/'       -->
+    <meta property="og:title" content="<?php echo he($app_name); ?>" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="<?php echo AppInfo::getUrl(); ?>" />
+    <meta property="og:image" content="<?php echo AppInfo::getUrl('/logo.png'); ?>" />
+    <meta property="og:site_name" content="<?php echo he($app_name); ?>" />
+    <meta property="og:description" content="" />
+    <meta property="fb:app_id" content="<?php echo AppInfo::appID(); ?>" />
+
+    <script type="text/javascript">
+      function logResponse(response) {
+        if (console && console.log) {
+          console.log('The response was', response);
+        }
+      }
+
+      $(function(){
+        // Set up so we handle click on the buttons
+        $('#postToWall').click(function() {
+          FB.ui(
+            {
+              method : 'feed',
+              link   : $(this).attr('data-url')
+            },
+            function (response) {
+              // If response is null the user canceled the dialog
+              if (response != null) {
+                logResponse(response);
+              }
+            }
+          );
+        });
+
+        $('#sendToFriends').click(function() {
+          FB.ui(
+            {
+              method : 'send',
+              link   : $(this).attr('data-url')
+            },
+            function (response) {
+              // If response is null the user canceled the dialog
+              if (response != null) {
+                logResponse(response);
+              }
+            }
+          );
+        });
+
+        $('#sendRequest').click(function() {
+          FB.ui(
+            {
+              method  : 'apprequests',
+              message : $(this).attr('data-message')
+            },
+            function (response) {
+              // If response is null the user canceled the dialog
+              if (response != null) {
+                logResponse(response);
+              }
+            }
+          );
+        });
+      });
+    </script>
+
+    <!--[if IE]>
+      <script type="text/javascript">
+        var tags = ['header', 'section'];
+        while(tags.length)
+          document.createElement(tags.pop());
+      </script>
+    <![endif]-->
+  </head>
+  <body>
+    <div id="fb-root"></div>
+    <script type="text/javascript">
+      window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '<?php echo AppInfo::appID(); ?>', // App ID
+          channelUrl : '//<?php echo $_SERVER["HTTP_HOST"]; ?>/channel.html', // Channel File
+          status     : true, // check login status
+          cookie     : true, // enable cookies to allow the server to access the session
+          xfbml      : true // parse XFBML
+        });
+
+        // Listen to the auth.login which will be called when the user logs in
+        // using the Login button
+        FB.Event.subscribe('auth.login', function(response) {
+          // We want to reload the page now so PHP can read the cookie that the
+          // Javascript SDK sat. But we don't want to use
+          // window.location.reload() because if this is in a canvas there was a
+          // post made to this page and a reload will trigger a message to the
+          // user asking if they want to send data again.
+          window.location = window.location;
+        });
+
+        FB.Canvas.setAutoGrow();
+      };
+
+      // Load the SDK Asynchronously
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/all.js";
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+    </script>
+
+    <header class="clearfix">
+      <?php if (isset($basic)) { ?>
+      <p id="picture" style="background-image: url(https://graph.facebook.com/<?php echo he($user_id); ?>/picture?type=normal)"></p>
+
+      <div>
+        <h1>Welcome, <strong><?php echo he(idx($basic, 'name')); ?></strong></h1>
+        <p class="tagline">
+          This is your app
+          <a href="<?php echo he(idx($app_info, 'link'));?>" target="_top"><?php echo he($app_name); ?></a>
+        </p>
+
+        <div id="share-app">
+          <p>Share your app:</p>
+          <ul>
+            <li>
+              <a href="#" class="facebook-button" id="postToWall" data-url="<?php echo AppInfo::getUrl(); ?>">
+                <span class="plus">Post to Wall</span>
+              </a>
+            </li>
+            <li>
+              <a href="#" class="facebook-button speech-bubble" id="sendToFriends" data-url="<?php echo AppInfo::getUrl(); ?>">
+                <span class="speech-bubble">Send Message</span>
+              </a>
+            </li>
+            <li>
+              <a href="#" class="facebook-button apprequests" id="sendRequest" data-message="Test this awesome app">
+                <span class="apprequests">Send Requests</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <?php } else { ?>
+      <div>
+        <h1>Welcome</h1>
+        <div class="fb-login-button" data-scope="user_likes,user_photos"></div>
+      </div>
+      <?php } ?>
+    </header>
 
 
+    <section id="player">
+      Persistant player
+    </section>
+
+    <section id="interact">
+
+    <?php if ($user_id): ?>
+      Chat box and share stuff
+
+    <?php else: ?>
+      Login with Facebook to interact with live chat and friends
+
+    <?php endif; ?>
+
+      Streaming chat...
+
+    </section>
+<!--
+    <section id="get-started">
+      <p>Welcome to your Facebook app, running on <span>heroku</span>!</p>
+      <a href="https://devcenter.heroku.com/articles/facebook" target="_top" class="button">Learn How to Edit This App</a>
+    </section>
+
+    <?php
+      if ($user_id) {
+    ?>
+
+    <section id="samples" class="clearfix">
+      <h1>Examples of the Facebook Graph API</h1>
+
+      <div class="list">
+        <h3>A few of your friends</h3>
+        <ul class="friends">
+          <?php
+            foreach ($friends as $friend) {
+              // Extract the pieces of info we need from the requests above
+              $id = idx($friend, 'id');
+              $name = idx($friend, 'name');
+          ?>
+          <li>
+            <a href="https://www.facebook.com/<?php echo he($id); ?>" target="_top">
+              <img src="https://graph.facebook.com/<?php echo he($id) ?>/picture?type=square" alt="<?php echo he($name); ?>">
+              <?php echo he($name); ?>
+            </a>
+          </li>
+          <?php
+            }
+          ?>
+        </ul>
+      </div>
+
+      <div class="list inline">
+        <h3>Recent photos</h3>
+        <ul class="photos">
+          <?php
+            $i = 0;
+            foreach ($photos as $photo) {
+              // Extract the pieces of info we need from the requests above
+              $id = idx($photo, 'id');
+              $picture = idx($photo, 'picture');
+              $link = idx($photo, 'link');
+
+              $class = ($i++ % 4 === 0) ? 'first-column' : '';
+          ?>
+          <li style="background-image: url(<?php echo he($picture); ?>);" class="<?php echo $class; ?>">
+            <a href="<?php echo he($link); ?>" target="_top"></a>
+          </li>
+          <?php
+            }
+          ?>
+        </ul>
+      </div>
+
+      <div class="list">
+        <h3>Things you like</h3>
+        <ul class="things">
+          <?php
+            foreach ($likes as $like) {
+              // Extract the pieces of info we need from the requests above
+              $id = idx($like, 'id');
+              $item = idx($like, 'name');
+
+              // This display's the object that the user liked as a link to
+              // that object's page.
+          ?>
+          <li>
+            <a href="https://www.facebook.com/<?php echo he($id); ?>" target="_top">
+              <img src="https://graph.facebook.com/<?php echo he($id) ?>/picture?type=square" alt="<?php echo he($item); ?>">
+              <?php echo he($item); ?>
+            </a>
+          </li>
+          <?php
+            }
+          ?>
+        </ul>
+      </div>
+
+      <div class="list">
+        <h3>Friends using this app</h3>
+        <ul class="friends">
+          <?php
+            foreach ($app_using_friends as $auf) {
+              // Extract the pieces of info we need from the requests above
+              $id = idx($auf, 'uid');
+              $name = idx($auf, 'name');
+          ?>
+          <li>
+            <a href="https://www.facebook.com/<?php echo he($id); ?>" target="_top">
+              <img src="https://graph.facebook.com/<?php echo he($id) ?>/picture?type=square" alt="<?php echo he($name); ?>">
+              <?php echo he($name); ?>
+            </a>
+          </li>
+          <?php
+            }
+          ?>
+        </ul>
+      </div>
+    </section>
+
+    <?php
+      }
+    ?>
+
+    <section id="guides" class="clearfix">
+      <h1>Learn More About Heroku &amp; Facebook Apps</h1>
+      <ul>
+        <li>
+          <a href="https://www.heroku.com/?utm_source=facebook&utm_medium=app&utm_campaign=fb_integration" target="_top" class="icon heroku">Heroku</a>
+          <p>Learn more about <a href="https://www.heroku.com/?utm_source=facebook&utm_medium=app&utm_campaign=fb_integration" target="_top">Heroku</a>, or read developer docs in the Heroku <a href="https://devcenter.heroku.com/" target="_top">Dev Center</a>.</p>
+        </li>
+        <li>
+          <a href="https://developers.facebook.com/docs/guides/web/" target="_top" class="icon websites">Websites</a>
+          <p>
+            Drive growth and engagement on your site with
+            Facebook Login and Social Plugins.
+          </p>
+        </li>
+        <li>
+          <a href="https://developers.facebook.com/docs/guides/mobile/" target="_top" class="icon mobile-apps">Mobile Apps</a>
+          <p>
+            Integrate with our core experience by building apps
+            that operate within Facebook.
+          </p>
+        </li>
+        <li>
+          <a href="https://developers.facebook.com/docs/guides/canvas/" target="_top" class="icon apps-on-facebook">Apps on Facebook</a>
+          <p>Let users find and connect to their friends in mobile apps and games.</p>
+        </li>
+      </ul>
+    </section>
+
+  -->
+  </body>
+</html>
--- schedule.php
+++ schedule.php
@@ -12,92 +12,113 @@ Shedular to get channel subscriptions
 
 
 
-/* mongodb */
+// Config
+$userId = "eatbassnow";
 
-try {
-// connect to MongoHQ assuming your MONGOHQ_URL environment
-// variable contains the connection string
-$connection_url = getenv("MONGOHQ_URL");
+echo 'Schedule';
 
-// create the mongo connection object
-$m = new Mongo($connection_url);
 
-// extract the DB name from the connection path
-$url = parse_url($connection_url);
-$db_name = preg_replace('/\/(.*)/', '$1', $url['path']);
 
-// use the database we connected to
-$db = $m->selectDB($db_name);
+  # get the mongo db name out of the env
+  $mongo_url = parse_url(getenv("MONGOHQ_URL"));
+  $dbname = str_replace("/", "", $mongo_url["path"]);
 
-echo "<h2>Collections</h2>";
-echo "<ul>";
+  # connect
+  $m   = new Mongo(getenv("MONGOHQ_URL"));
+  $db  = $m->$dbname;
+  $col = $db->videos;
 
-// print out list of collections
-$cursor = $db->listCollections();
-$collection_name = "";
-foreach( $cursor as $doc ) {
-  echo "<li>" .  $doc->getName() . "</li>";
-  $collection_name = $doc->getName();
+  # insert a document
+  //$visit = array( "ip" => 'blergy' );
+  //$col->insert($visit);
+
+
+
+
+
+  // Cycle through subscriptions and videos
+  $subscriptions = _api_request("https://gdata.youtube.com/feeds/api/users/$userId/subscriptions?v=2&alt=json&limit=10");
+
+  foreach($subscriptions->feed->entry as $subscription) {
+      //var_dump($subscription);
+
+      $channelId = $subscription->{'yt$username'}->{'$t'};
+
+      $videos = _api_request("http://gdata.youtube.com/feeds/api/users/$channelId/uploads?v=2&alt=json");
+
+      //http://gdata.youtube.com/feeds/api/users/nba/uploads/-/sports/playoffs?v=2&alt=json
+
+      foreach($videos->feed->entry as $video) {
+          //var_dump($video);
+          //echo $video->title->{'$t'} . "<br />";
+
+          //die(_mongo_insert('videos', (array) $video));
+          //$col->insert($video);
+
+          $video->_id = $video->id->{'$t'};
+
+          try {
+              $col->insert($video, true);
+          } catch(MongoCursorException $e) {
+              echo "Can't save the same person twice!\n";
+          }
+      }
+  }
+
+
+
+
+  # print all existing documents
+  $data = $col->find();
+  foreach($data as $video) {
+    echo "<li>" . $video['title']['$t'] . "</li>";
+  }
+
+  # disconnect
+  $m->close();
+
+
+/*
+
+
+
+
+*/
+
+
+
+
+
+// Functions
+
+function _api_request($url) {
+    return json_decode(file_get_contents($url));
 }
-echo "</ul>";
 
-// print out last collection
-if ( $collection_name != "" ) {
-  $collection = $db->selectCollection($collection_name);
-  echo "<h2>Documents in ${collection_name}</h2>";
+/*
 
-  // only print out the first 5 docs
-  $cursor = $collection->find();
-  $cursor->limit(5);
-  echo $cursor->count() . ' document(s) found. <br/>';
-  foreach( $cursor as $doc ) {
-    echo "<pre>";
-    var_dump($doc);
-    echo "</pre>";
+
+function _mongo_insert($collection, $array)
+{
+  try {
+    $connection_url = getenv("MONGOHQ_URL");
+
+    // create the mongo connection object
+    $m = new Mongo($connection_url);
+    $db = $m->selectDB('app9314556');
+
+    $collection = new MongoCollection($m, $collection);
+    ////
+
+    $m->close();
+  } catch ( MongoConnectionException $e ) {
+    die('Error connecting to MongoDB server');
+  } catch ( MongoException $e ) {
+    die('Mongo Error: ' . $e->getMessage());
+  } catch ( Exception $e ) {
+    die('Error: ' . $e->getMessage());
   }
 }
 
-// disconnect from server
-$m->close();
-} catch ( MongoConnectionException $e ) {
-die('Error connecting to MongoDB server');
-} catch ( MongoException $e ) {
-die('Mongo Error: ' . $e->getMessage());
-} catch ( Exception $e ) {
-die('Error: ' . $e->getMessage());
-}
+*/
 
-
-
-
-
-
-
-
-// https://www.googleapis.com/youtube/v3/subscriptions
-$userId = "eatbassnow";
-
-
-
-// Cycle through subscriptions and videos
-$subscriptions = _apiRequest("https://gdata.youtube.com/feeds/api/users/$userId/subscriptions?v=2&alt=json");
-
-foreach($subscriptions->feed->entry as $subscription) {
-    //var_dump($subscription);
-
-    $channelId = $subscription->{'yt$username'}->{'$t'};
-
-    $videos = _apiRequest("http://gdata.youtube.com/feeds/api/users/$channelId/uploads?v=2&alt=json");
-
-    //http://gdata.youtube.com/feeds/api/users/nba/uploads/-/sports/playoffs?v=2&alt=json
-
-    foreach($videos->feed->entry as $video) {
-        die(var_dump($video));
-        //echo $video->title->{'$t'} . "<br />";
-    }
-}
-
-
-function _apiRequest($url) {
-    return json_decode(file_get_contents($url));
-}
