9c8152a32757d8a6836406d382fda44cb67ffe61
Ben Bowler <ben@benbowler.com>
2012-11-28 10:12:45 +0000

    Do not enforce ssl

--- app/app.php
+++ app/app.php
@@ -13,10 +13,12 @@
 require_once('AppInfo.php');
 
 // Enforce https on production
+/*
 if (substr(AppInfo::getUrl(), 0, 8) != 'https://' && $_SERVER['REMOTE_ADDR'] != '127.0.0.1') {
   header('Location: https://'. $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
   exit();
 }
+*/
 
 // This provides access to helper functions defined in 'utils.php'
 require_once('utils.php');
