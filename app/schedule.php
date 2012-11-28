bda8286dcc7e7be92d7516a3051938383d2cd342
Ben Bowler <ben@benbowler.com>
2012-11-28 12:55:04 +0000

    Full page text substitutio

--- app/schedule.php
+++ app/schedule.php
@@ -1,7 +1,7 @@
 <?php
 // Shedular to get channel subscriptions
 
-if(!preg_match('eatbass', $_SERVER['HTTP_HOST'])) {
+if(!strstr('eatbass', $_SERVER['HTTP_HOST'])) {
   die(file_get_contents('http://eatbass.com/app/schedule.php'));
 }
 
