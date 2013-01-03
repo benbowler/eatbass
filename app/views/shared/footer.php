4d251e9e3d0e7b6c8c485038e601fa65918d8df1
Ben Bowler <ben@benbowler.com>
2013-01-03 17:27:38 +0000

    Fixed relative links

--- app/views/shared/footer.php
+++ app/views/shared/footer.php
@@ -1,9 +1,9 @@
 <footer>
-	<a href="info:privacy-policy" target="_blank">privacy policy</a>
-	<a href="info:terms-of-service" target="_blank">terms of service</a>
-	<a href="info:winning" target="_blank">winning</a>
-	<a href="info:channels" target="_blank">channels</a>
-	<a href="info:support" tartget="_blank">support</a>
+	<a href="/info:privacy-policy" target="_blank">privacy policy</a>
+	<a href="/info:terms-of-service" target="_blank">terms of service</a>
+	<a href="/info:winning" target="_blank">winning</a>
+	<a href="/info:channels" target="_blank">channels</a>
+	<a href="/info:support" tartget="_blank">support</a>
 	<a href="https://facebook.com/eatbassnow" target="_blank">facebook</a>
 	<a href="https://twitter.com/eatbassnow" target="_blank">twitter</a>
 </footer>
