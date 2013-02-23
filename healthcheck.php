34d44603cd6cc0aa094cac7f55ecfc3040738d21
Ben Bowler <ben@benbowler.com>
2013-02-23 15:43:58 +0000

    Fixed healthcheck file

--- healthcheck.php
+++ healthcheck.php
@@ -1,12 +1,15 @@
 <?php
+# get the mongo db name out of the env
+$mongo_url = parse_url(getenv("PARAM3"));
+$dbname = str_replace("/", "", $mongo_url["path"]);
 
 # connect
-$this->m   = new Mongo(getenv("PARAM3"));
-$this->db  = $this->m->{$this->dbname};
+$m   = new Mongo(getenv("PARAM3"));
+$db  = $m->{$dbname};
 
 
-$this->col = $this->db->videos;
-$video = $this->col->findOne(array('featured' => true));
+$col = $db->videos;
+$video = $col->findOne(array('featured' => true));
 
 if($video) {
 	echo 'up';
